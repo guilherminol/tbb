67764e53e87ac2f244000fa4e1b0b1e817f44649
Guilhermino Lucas <guilherminolucasc@gmail.com>
2022-03-04 17:04:16 -0300

    Fix: getting the ul better

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,6 +1,8 @@
 import { Buttons, Container } from "./style";
 import { BsCart4 } from "react-icons/bs";
 import parse from "html-react-parser";
+import { useNavigate } from "react-router-dom";
+
 interface Product {
   name: string;
   shortDescription: string;
@@ -19,13 +21,16 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const navigate = useNavigate();
   return (
     <Container>
       <h3>{product.name}</h3>
       <img src={product.images[0].src} alt={product.images[0].alt} />
       <p>{parse(product.shortDescription)}</p>
       <Buttons>
-        <button className="Details">Ver mais</button>
+        <button className="Details" onClick={() => navigate(product.id)}>
+          Ver mais
+        </button>
         <button className="Cart">{<BsCart4 />}</button>
       </Buttons>
     </Container>
--- src/components/productsList/style.ts
+++ src/components/productsList/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.ul`
-  padding: 0;
+    padding: 0
   margin: 0 auto;
   display: grid;
   justify-content: space-around;
@@ -10,6 +10,8 @@ export const Container = styled.ul`
   grid-template-columns: repeat(auto-fill, 320px);
   list-style-type: none;
   @media (min-width: 900px) {
+    padding: 0 30px;
+
     grid-template-columns: repeat(auto-fill, 430px);
   }
 `;
