2bb252c437e09b1494fe70486484edf705a7fa93
Guilhermino Lucas <guilherminolucasc@gmail.com>
2022-03-04 12:20:00 -0300

    Feat: adding the normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./normalize.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
